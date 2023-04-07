b40ea958d855d6ae33a5384010376bcc9db57c66
azer <azerchabbar@gmail.com>
2023-04-07 04:34:44 +0100

    syntax analyzer

---
+++ syntax_analyzer/test_examples/Test2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Test2 {
+    /* this is the main function
+        that will be called
+    */
+    public static void main(String[] args) {
+        this.degla = new Degla();
+        boolean _ba3$ = true;
+        int num$aux = 15;
+        int z = x / y;
+        int w = x&y;
+        bool x = false;
+        String wrong = "asdf asd fasd;
+        System.out.println("The result is " + z);
+    } 
+}
